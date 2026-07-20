47e409bb9d7aa77fc958aaf541fef20bd64a3294
Honey-Rajput <honeyrajputmahiyaan@gmail.com>
2026-07-20 18:38:54 +0530

    Fix 'Save Results' ignoring multiple trend setups, and fix emoji crash in cron_scanner

--- vdu_scanner/cron_scanner.py
+++ vdu_scanner/cron_scanner.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import io
+if sys.platform == "win32":
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 import time
 import random
 import concurrent.futures
